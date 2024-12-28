49b16d394357b437643eee9565c895f5940c902f
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-12-28 21:14:04 +0100

    ml part 2

--- 2/nn.c
+++ 2/nn.c
@@ -1,25 +1,71 @@
+#include <stddef.h>
 #include <stdio.h>
 #define NN_IMPLEMENTATION
 #include "nn.h"
 #include <time.h>
 
+float td_xor[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 0,
+};
+
+float td_or[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 1,
+};
+
 int main(void) {
     srand(time(0));
 
-    Mat w1 = mat_alloc(2, 2);
-    Mat b1 = mat_alloc(1, 2);
-    Mat w2 = mat_alloc(2, 1);
-    Mat b2 = mat_alloc(1, 1);
+    size_t stride = 3;
+    size_t n = 4;
 
-    mat_rand(w1, 0, 1);
-    mat_rand(b1, 0, 1);
-    mat_rand(w2, 0, 1);
-    mat_rand(b2, 0, 1);
+    float *td = td_xor;
 
-    MAT_PRINT(w1);
-    MAT_PRINT(b1);
-    MAT_PRINT(w2);
-    MAT_PRINT(b2);
+    float eps = 1e-1;
+    float rate = 1e-1;
+
+    Mat ti = {
+        .rows = n,
+        .cols = 2,
+        .stride = stride,
+        .es = td,
+    };
+
+    Mat to =  {
+        .rows = n,
+        .cols = 1,
+        .stride = stride,
+        .es = td + 2
+    };
+
+    size_t arch[] = {2, 3, 1};
+    NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+    NN g = nn_alloc(arch, ARRAY_LEN(arch));
+    nn_rand(nn, 0, 1);
+
+    printf("cost = %f\n", nn_cost(nn, ti, to));
+    for (size_t i = 0; i < 20*1000; ++i) {
+        nn_finite_diff(nn, g, eps, ti, to);
+        nn_learn(nn, g, rate);
+        printf("cost = %f\n", nn_cost(nn, ti, to));
+    }
+
+    NN_PRINT(nn);
+
+    printf("---------\n");
+    for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+            MAT_AT(NN_INPUT(nn), 0, 0) = i;
+            MAT_AT(NN_INPUT(nn), 0, 1) = j;
+            nn_forward(nn);
+            printf("%zu ^ %zu = %f\n", i, j, MAT_AT(NN_OUTPUT(nn), 0, 0));
+        }
+    }
 
     return 0;
 }
